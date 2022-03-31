8345250aa4c2d3ae5efe573aa17459d28a093817
sampson-q <quarmysokpoli@gmail.com>
2022-03-31 02:23:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7yfzjv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7yfzjv)
---
+++ src/app/boutiqueproducts.ts
@@ -0,0 +1,23 @@
+export interface BoutiqueProducts {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const boutiqueproducts = [
+  {
+    id: 1,
+    name: 'Spoiler Green',
+    price: 500,
+    description: 'Best dress ever 0',
+  },
+  {
+    id: 2,
+    name: "Devi's Gown",
+    price: 8500,
+    description: 'Right from the Devil',
+  },
+  { id: 3, name: 'Press Wide', price: 300, description: 'Paris Made' },
+  { id: 4, name: 'Batakali', price: 1000, description: 'African Smoke' },
+];
